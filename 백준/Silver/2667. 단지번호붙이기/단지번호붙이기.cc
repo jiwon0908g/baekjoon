52d6216c3674f80fca24e3f9c2217fccee65cfa5
jiwon0908g <141632359+jiwon0908g@users.noreply.github.com>
2024-03-16 23:58:42 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> dx = {-1,0,1,0};
+vector<int> dy = {0,-1,0,1};
+
+int cnt = 0;
+void dfs(int x,int y, vector<vector<char>> &a, vector<vector<int>> &v) {
+    v[x][y] = 1;
+    for(int i=0;i<4;i++) {
+        int tmpx = x+dx[i];
+        int tmpy = y+dy[i];
+        if(v[tmpx][tmpy]==0&&a[tmpx][tmpy]=='1') dfs(tmpx,tmpy,a,v);
+    }
+    cnt++;
+}
+
+int main() {
+    int n,i,j;
+    cin >> n;
+    vector<vector<char>> arr(n+2, vector<char> (n+2));
+    vector<vector<int>> visit(n+2, vector<int> (n+2));
+    vector<int> ans;
+
+    for(i=1;i<=n;i++) {
+        for(j=1;j<=n;j++) {
+            cin >>arr[i][j];
+        }
+    }
+
+    for(i=1;i<=n;i++) {
+        for(j=1;j<=n;j++) {
+            cnt = 0;
+            if(arr[i][j]=='1'&&visit[i][j]==0) {
+                dfs(i,j,arr,visit);
+                ans.push_back(cnt);
+            }
+        }
+    }
+
+    sort(ans.begin(), ans.end());
+    cout << ans.size()<<"\n";
+    for(i=0;i<ans.size();i++) {
+        cout << ans[i]<<"\n";
+    }
+
+    return 0;
+}
